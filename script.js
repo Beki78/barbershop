724f39dc63a3b6d6087bbc2895bb4e058acc0b36
Bereket Getachew <146061009+Beki78@users.noreply.github.com>
2024-02-16 22:49:08 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function openMenue() {
+  const sideBar = document.querySelector(".sideBar");
+
+  sideBar.style.display = "flex";
+}
+function closeMenue() {
+  const close = document.querySelector(".close");
+  const sideBar = document.querySelector(".sideBar");
+
+  sideBar.style.display = "none";
+}
+ 
